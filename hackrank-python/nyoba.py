7f823b1706c096898aece3bd68ae685c40636135
sitifatim <99231159+sitifatim@users.noreply.github.com>
2023-03-22 21:58:14 +0700

    create hackerrank folder

---
+++ hackrank-python/nyoba.py
@@ -0,0 +1,11 @@
+x = 1
+y=1
+z=1
+
+list1=[]
+
+
+for i in range(x+1):
+    for j in range(y+1):
+        for k in range(z+1):
+            list1.append([i,j,k])
---
+++ hackrank-python/print function.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    n = int(input())
+
+    temp = 1
+    for i in range(n):
+        j = temp + i
+        print(j, end="")
