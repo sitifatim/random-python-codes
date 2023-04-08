1fa145833f50257213c33314d36260ea3d1abde2
sitifatim <99231159+sitifatim@users.noreply.github.com>
2023-04-09 00:26:07 +0700

    add new study

---
+++ study/.vscode/tipe data.py
@@ -0,0 +1,28 @@
+# tipe data integer (angka)
+data_int = 2
+print("data: ", data_int, "bertipe data: ", type(data_int))
+
+#tipe data float (pecahan/berkoma)
+data_float = 3.5
+print("data: ", data_float)
+print("- bertipe: ", type(data_float))
+
+#tipe data string (karakter)
+data_string = "cie"
+print("data: ", data_string)
+print("- bertipe: ", type(data_string))
+
+#tipe data boolean
+data_bool = True
+print("data: ", data_bool)
+print("- bertipe: ", type(data_bool))
+
+#tipe data kompleks
+
+data_kompleks = complex(3,6)
+print("data: ", data_kompleks)
+print("- bertipe: ", type(data_kompleks))
+
+
+
+
---
+++ study/.vscode/variabel.py
@@ -0,0 +1,18 @@
+#variabel adalah tempat menyimpan data
+
+#assigment nilai/menugaskan nilai pada variabel
+a=15
+b=4
+panjang = 1000
+
+print("Nilai a =", a)
+print("Nilai b = ",b)
+
+#penamaan variabel
+nilai_c = 20 #menggunakan underscore
+ribu10 = 40000 #angka di belakan huruf
+nilaiZ = 23.5
+
+#assigment indirect
+nilai_c=panjang
+print("Nilai nilai_c=", nilai_c)
