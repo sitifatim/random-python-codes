5f5a79ad65b9163e1d74affc060f3f1a29757262
sitifatim <sitifatim0010@gmail.com>
2023-06-30 00:19:45 +0700

    change the code

--- hackrank-python/wrap text.py
+++ hackrank-python/wrap text.py
@@ -1,11 +1,16 @@
-string = ['A','B','C','D','E']
-max = 4
-a = ''
-b = []
-for i,v in enumerate(string):
-    a = ''.join(v)
-    # if a == (max-1):
-    #     print(a)
+import textwrap
 
-print(a)
+def wrap(string, max_width):
+    text = textwrap.fill(string, max_width)
+    return text
 
+''' textwrap is module for wrapping text. as for fill is stand from
+    "\n".join(wrap(string, width)) so if you want to separate list of words with certain 
+    length, you can easily use this modul instead of spending time to creating 
+    unnecessary logic
+'''
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
